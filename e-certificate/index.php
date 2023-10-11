eebf672e770e4628b36294015aed3ac2e62f3334
Yuttapong Napikun <zamoko@hotmail.com>
2023-10-11 07:15:00 +0700

    test

--- e-certificate/index.php
+++ e-certificate/index.php
@@ -1,2 +1,10 @@
 <?php 
+$pdo = new \PDO("mysql:host=mysql;dbname=tpacal;port=3306;charset=utf8", 'user', '1234');
+$pdo->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+
+$rows = $pdo->query("SELECT * FROM provinces");
+foreach ($rows as $row) {
+    echo $row['name_th'] . PHP_EOL;
+}
+
 echo phpinfo();
--- ganban/index.js
+++ ganban/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.end("Hello, World");
+  res.end("Hello, World ! 555555");
 });
 
 const PORT = process.env.PORT || 3000;
